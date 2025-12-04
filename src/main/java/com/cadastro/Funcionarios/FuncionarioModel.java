960820b3904d88e29aadac51d6b47be238b9b613
j-adenilson <j.adenilson@outlook.com.br>
2025-12-03 23:15:06 -0300

    fix: coloca a coluna email como (unique = true)

--- src/main/java/com/cadastro/Funcionarios/FuncionarioModel.java
+++ src/main/java/com/cadastro/Funcionarios/FuncionarioModel.java
@@ -17,8 +17,12 @@ public class FuncionarioModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
    private Long id;
+
    private String nome;
+
+   @Column(unique = true)
    private String email;
+
    private int idade;
 
    @ManyToOne //um funcionario tem um unico departamento
